2e88b1df01b72332c6ead70a847b25e9fee38484
IANOONE <86506939+IANOONE@users.noreply.github.com>
2023-02-07 15:38:54 +0700

    Update product.js

--- src/routes/product.js
+++ src/routes/product.js
@@ -6,10 +6,13 @@ const {productValidateRules, validate} = require("../middlewares/validator")
 // const {validateUser, userValidationRules} = require("../middlewares/authValidator")
 const fileUploader = require("../middlewares/multer")
 const uploadSingle = fileUploader({destinationFolder : "products_img"}).single('image')
+const { tokenVerify } = require("../middlewares/verifyToken")
 
 
 router.post("/create", productValidateRules(), validate, uploadSingle , productController.create)
 router.patch("/edit/:id", uploadSingle , productController.edit)
 router.get("/list", productController.fetchProduct)
+router.get("/byCategory/:id", productController.fetchProductByCatId)
+router.delete("/delete/:id", productController.delete)
 
 module.exports = router
